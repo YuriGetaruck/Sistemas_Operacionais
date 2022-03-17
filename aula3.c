2d7e79394a9a2df502ecbf763ee2d85d25958b47
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2022-03-17 08:50:20 -0300

    Update aula3.c

--- aula3.c
+++ aula3.c
@@ -11,7 +11,7 @@ int resposta = 0;
 
 void handler_c(){
 
-    printf("\nvoce apertou ctrl C\n");
+    //printf("\nvoce apertou ctrl C\n");
     
     contador++;
 
@@ -19,13 +19,13 @@ void handler_c(){
 
 void handler_z(){
 
-    printf("\nvoce apertou ^C '%d' vezes\n", contador);
+    printf("\nvoce apertou ^C '%i' vezes\n", contador);
 
 }
 
 void alarmHandler(){
 
-    printf("\n se passaram 20 segundos e o programa ira finalizar \n");
+    printf("\nSe passaram 20 segundos e o programa ira finalizar \n");
     controle = 0;
 }
 
@@ -37,13 +37,16 @@ int main(){
     signal(SIGTSTP, handler_z);
     signal(SIGALRM, alarmHandler);
 
+
+
     while(controle){
+
         
         if (contador >= 3){
 
-            printf("realmente deseja sair do programa?[1/0]");
-            alarm(10);
-            //scanf("%d",&resposta);
+            printf("\nrealmente deseja sair do programa?[1/0]:");
+            alarm(20);
+            scanf("%d",&resposta);
             
             if(resposta==1){
                 controle = 0;
@@ -55,7 +58,7 @@ int main(){
         }
     }
     
-    printf("obrigado\n");
+    printf("\nobrigado\n");
 
     return 0;
 }
