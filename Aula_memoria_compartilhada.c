8439008bb8f25a93bc1eec5859d461c06f31f517
Yuri Constantino Getaruck <90275932+YuriGetaruck@users.noreply.github.com>
2022-04-04 11:41:44 -0300

    Add files via upload

---
+++ Aula_memoria_compartilhada.c
@@ -0,0 +1,40 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/shm.h>
+
+#define SHM_SIZE 1024
+
+int main(int argc, char* argv[]){
+
+    key_t key;
+    int shmid;
+    char *data;
+
+    if((key = ftok("/home", 'A')) == -1){
+        perror("Ftok");
+        exit(1);
+    }
+
+    if((shmid = shmget(key, SHM_SIZE, 766 | IPC_CREAT))==-1){
+        perror("Shmget");
+        exit(1);
+    }
+
+    data = (char *)shmat(shmid, (void *)0,0);
+
+    if(argc == 2){
+        strncpy(data, argv[1], SHM_SIZE);
+
+
+    }else{
+        printf("o segmento contem %s \n\n", data);
+    }
+
+    shmdt(data);
+    shmctl(shmid, IPC_RMID, NULL);
+    
+    return 0;
+}
